631c4f7dc0fa3cb477dea27c7acf448b897eb327
reham ayman <rehamayman15@icloud.com>
2020-05-28 05:23:03 +0200

    todo list complete with table view controller

---
+++ TTOODOlist/TODOTabelViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  TTOODOlist
+//
+//  Created by Reham Ayman on 5/28/20.
+//  Copyright © 2020 Reham Ayman. All rights reserved.
+//
+
+import UIKit
+
+class TODOTabelViewController: UITableViewController{
+    let itemarray = [ "item1" , "item 2 " , "item 3 " , "item4 "]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemarray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TODOITEMCELL", for: indexPath)
+        cell.textLabel?.text = itemarray[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemarray[indexPath.row])
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+    }
+}
+
